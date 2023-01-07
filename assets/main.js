69149d04dda22e8a87e51ff45bd160693dd98d9c
Artiren <sketchhydrogenmagnesium@gmail.com>
2023-01-07 16:23:13 +0900

    add theme toggle

--- assets/main.js
+++ assets/main.js
@@ -4,8 +4,7 @@ if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
     document.body.dataset.theme = 'light-mode'
 }
 
-const darkmodeBtn = document.querySelector('.darkmode__btn')
-darkmodeBtn.addEventListener('click', () => {
+function toggleDarkMode() {
     let dataAttr = document.body.dataset;
-    dataAttr.theme.match('light-mode') ? dataAttr.theme = 'dark-mode' : dataAttr.theme = 'light-mode'
-});
+    dataAttr.theme.match('light-mode') ? dataAttr.theme = 'dark-mode' : dataAttr.theme = 'light-mode';
+}
