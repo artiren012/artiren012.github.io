d57cf649d0a7375b22fb9ea5ea2917938216c1e9
Artiren <sketchhydrogenmagnesium@gmail.com>
2023-01-08 15:18:21 +0900

    add about, add theme parameter

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,35 @@
+let isDark = false;
+
 if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    isDark = true;
+}
+
+window.onload = function () {
+    var url = decodeURI(location.href);
+    var pm = url.split('?');
+    var params = pm[1].split('&');
+    for (var i = 0; i < params.length; i++) {
+        var param = params[i].split('=');
+        value = param[1];
+    }
+    if (value == 'dark') {
+        isDark = true;
+    }
+}
+
+if (isDark) {
     document.body.dataset.theme = 'dark-mode'
 } else {
     document.body.dataset.theme = 'light-mode'
 }
 
-const darkmodeBtn = document.querySelector('.darkmode-toggle');
-darkmodeBtn.addEventListener('click', () => {
+function toggleDarkMode() {
     let dataAttr = document.body.dataset;
     dataAttr.theme.match('light-mode') ? dataAttr.theme = 'dark-mode' : dataAttr.theme = 'light-mode';
-});
+}
+
+function loadPage(link) {
+    let mode = 'light';
+    if (isDark) mode = 'dark';
+    location.href = link + '?theme=' + mode;
+}
