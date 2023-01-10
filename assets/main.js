7ad62828458fe7b3a5befc170ace3f669c574cad
Artiren <sketchhydrogenmagnesium@gmail.com>
2023-01-10 15:29:55 +0900

    add /project, remove theme parameter

--- assets/main.js
+++ assets/main.js
@@ -1,23 +1,5 @@
-let isDark = false;
 
 if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    isDark = true;
-}
-
-window.onload = function () {
-    var url = decodeURI(location.href);
-    var pm = url.split('?');
-    var params = pm[1].split('&');
-    for (var i = 0; i < params.length; i++) {
-        var param = params[i].split('=');
-        value = param[1];
-    }
-    if (value == 'dark') {
-        isDark = true;
-    }
-}
-
-if (isDark) {
     document.body.dataset.theme = 'dark-mode'
 } else {
     document.body.dataset.theme = 'light-mode'
@@ -27,10 +9,4 @@ function toggleDarkMode() {
     let dataAttr = document.body.dataset;
     dataAttr.theme.match('light-mode') ? dataAttr.theme = 'dark-mode' : dataAttr.theme = 'light-mode';
     isDark = !isDark;
-}
-
-function loadPage(link) {
-    let mode = 'light';
-    if (isDark) mode = 'dark';
-    location.href = link + '?theme=' + mode;
 }
